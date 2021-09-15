466e520007b91ee2fc0533f9e5237d603c9090ab
Theo Ferguson <ferguson.theo@gmail.com>
2021-09-15 19:19:37 -0400

    commit tree cc3e901e6848753a181d53f005b09993096f0798

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,22 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <h1>Directors Page</h1>
+    {directors.map(director => {
+      return (
+        <div id={director.name} >{director.name}
+          <ul>
+            {director.movies.map(movie => {
+              return (
+                <li>{movie}</li>
+              )
+            })}
+          </ul>
+        </div>
+      )
+    })}
+  </div>;
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,22 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <h1>Movies Page</h1>
+  {movies.map(movie =>{
+    return (
+      <div id={movie.time} >{movie.title} {movie.time}
+      <ul>
+        {movie.genres.map(genre => {
+          return (
+            <li id={genre}>{genre}</li>
+          )
+        })}
+      </ul>
+      </div>
+    )
+  })} 
+  </div>;
 }
 
 export default Movies;
